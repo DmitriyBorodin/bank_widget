2599696e57241617a4f9f91abaf82b2b28e16baa
Dmitriy Borodin <dmitriyborodinpm@gmail.com>
2024-04-24 21:34:40 +0300

    Написал тест для sort_operations_by_date

--- tests/test_func.py
+++ tests/test_func.py
@@ -11,3 +11,21 @@ def test_get_executed_operations():
         {"state": "executed"}
     ]
     assert get_executed_operations(operations) == [{"state": "EXECUTED"}, {"state": "EXECUTED"}]
+
+
+def test_sort_operations_by_date():
+    operations = [
+        {"date": "2019-04-14T23:10:21.514616"},
+        {"date": "2018-12-29T21:45:18.495053"},
+        {"date": "2019-04-14T19:37:49.044089"},
+        {"date": "2017-10-30T01:49:52.939296"}
+    ]
+
+    sorted_operations = sort_operations_by_date(operations)
+
+    assert sorted_operations == [
+        {"date": "2019-04-14T23:10:21.514616"},
+        {"date": "2019-04-14T19:37:49.044089"},
+        {"date": "2018-12-29T21:45:18.495053"},
+        {"date": "2017-10-30T01:49:52.939296"}
+    ]
